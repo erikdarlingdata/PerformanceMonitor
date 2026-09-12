9f4f80ec167c50bdf09a55c316bb58d2022ecb03
Erik Darling <2136037+erikdarlingdata@users.noreply.github.com>
2026-09-11 20:24:34 -0400

    Drop the unused using the pin added, and sort the rest
    Review nit. System.Collections.Generic came in with the first spelling of
    the pin, which used a List, and stayed behind when the phrase-based version
    replaced it. The added lines also put System.Text.RegularExpressions ahead
    of System.Reflection.

--- Darling/Darling.Tests/BuiltinAlertPersistenceRungTests.cs
+++ Darling/Darling.Tests/BuiltinAlertPersistenceRungTests.cs
@@ -7,12 +7,11 @@
  */
 
 using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using PerformanceMonitor.Alerting;
 using PerformanceMonitor.Common;
 using PerformanceMonitor.Darling.Storage;
